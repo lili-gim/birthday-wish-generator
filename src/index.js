b041bdb8c82390c0318dcd0c355ac3c83c57a6f4
lili-gim <156603091+lili-gim@users.noreply.github.com>
2024-07-09 20:51:42 +0200

    Integrated API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-function showWish(event) {
-  event.preventDefault();
-
+function showWish(response) {
   new Typewriter("#wish", {
-    strings: "Happy Birthday",
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 1,
   });
 }
 
-let wishForm = document.querySelector("#form");
-wishForm.addEventListener("click", showWish);
+function generateWish(event) {
+  event.preventDefault();
+  let keyInput = document.querySelector("#key-wish-words");
+
+  let apiKey = "c20161a98fff44a06b23236e2obd0t3b";
+  let prompt = `Generate a witty birtday wish including ${keyInput.value}. `;
+  let context =
+    "you are the best at creating witty birthday wishes with the provided words";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(generateWish);
+
+  axios.get(apiUrl).then(showWish);
+}
+
+let wishForm = document.querySelector("#wish-form");
+wishForm.addEventListener("submit", generateWish);
